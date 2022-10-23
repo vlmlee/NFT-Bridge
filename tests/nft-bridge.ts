0912dda9b5d3db1fa090f75cb46e6ed294b572b3
Michael Lee <vlmlee2@gmail.com>
2022-10-23 16:53:56 -0500

    Added prettier

--- tests/nft-bridge.ts
+++ tests/nft-bridge.ts
@@ -1,16 +1,16 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { NftBridge } from "../target/types/nft_bridge";
+import {Program} from "@project-serum/anchor";
+import {NftBridge} from "../target/types/nft_bridge";
 
 describe("nft-bridge", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster.
+    anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.NftBridge as Program<NftBridge>;
+    const program = anchor.workspace.NftBridge as Program<NftBridge>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    it("Is initialized!", async () => {
+        // Add your test here.
+        const tx = await program.methods.initialize().rpc();
+        console.log("Your transaction signature", tx);
+    });
 });
